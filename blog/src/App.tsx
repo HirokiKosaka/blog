70acba5ae2cf396bde4839d8f6fd5cf833e16803
arp.kosaka <h.kosaka.arp@gmail.com>
2024-11-10 21:14:17 +0900

    create top

--- blog/src/App.tsx
+++ blog/src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import './App.css'
+import React from "react";
 
 function App() {
-
   return (
     <>
+      <h1 className="text-white">Blog</h1>
+      <p className="text-white">SignIn</p>
+      <p className="text-white">Login</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
